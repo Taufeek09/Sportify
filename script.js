6b366bc2f53393b298abecc90a05597f4b59597e
Taufeek09 <taufeek09sk@gmail.com>
2024-02-28 14:36:49 +0530

    change js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("hello world");
+
+async function getSongs() {
+
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let responce = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = responce;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mpeg")) {
+            songs.push(element.href.split("/songs/")[1])
+        }
+
+    }
+    return songs
+
+}
+
+async function main() {
+
+    let songs = await getSongs()
+    console.log(songs);
+
+   let songUL =  document.querySelector(".songlist").getElementsByTagName("ul")[0]
+   for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20"," ")} </li>`;
+    
+   }
+
+    var audio = new Audio(songs[0]);
+    audio.play();
+}
+main()
